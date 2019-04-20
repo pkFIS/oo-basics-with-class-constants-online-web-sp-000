3058b0c34849d36236a1239b12b49cfc1918cebe
Paul K <pkfis@outlook.com>
2019-04-20 22:24:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
@@ -15,9 +18,7 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    if BRANDS.include?(brand)
-      BRANDS << brand
-    end
+
   end
 
 end
